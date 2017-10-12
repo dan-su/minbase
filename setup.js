ef3a518b910f82da618c60a5187532ae047ea77f
Ev Bogue <ev@evbogue.com>
2017-10-12 15:36:40 -0700

    make sure to stringify key before printing

--- setup.js
+++ setup.js
@@ -5,8 +5,9 @@ var ws = require('ssb-ws')
 var chalk = require('chalk')
 
 client(function (err, sbot) {
+  printKey = JSON.stringify(key).replace(/\s+/g)
   console.log('\nThis is your public/private key, paste it into the `key` field at ' +  chalk.cyan('http://localhost:3013/#Key') + '\n')
-  console.log(key)
+  console.log(printKey)
   console.log('\nThis is your web socket remote, paste it into the `remote` field at ' + chalk.cyan('http://localhost:3013/#Key') + '\n')
   sbot.ws.getAddress(function (err, data) { console.log(data + '\n')})
   sbot.close()
