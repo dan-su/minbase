1694b4c52b98e7138acf2a2fccbe5590272797f3
Ev Bogue <ev@evbogue.com>
2018-03-03 21:35:33 -0600

    make avatar image work with new required sbot

--- modules/avatar-image.js
+++ modules/avatar-image.js
@@ -10,7 +10,7 @@ var query = require('./scuttlebot').query
 var blobUrl = require('./helpers').bloburl
 
 exports.gives = {
-  connection_status: true, avatar_image: true
+  avatar_image: true
 }
 
 function isFunction (f) {
@@ -25,79 +25,79 @@ var last = 0
 var cache = {}
 
 exports.create = function (api) {
+  var exports = {}
   var avatars  = {}
-  return {
-    connection_status: function (err) {
-      if (err) return
-      pull(
-        query({
-          query: [{
-            $filter: {
-              timestamp: {$gt: last || 0 },
-              value: { content: {
-                type: "about",
-                about: {$prefix: "@"},
-                image: {$truthy: true}
-            }}
-          }},
-          {
-            $map: {
-              id: ["value", "content", "about"],
-              image: ["value", "content", "image"],
-              by: ["value", "author"],
-              ts: 'timestamp'
-          }}],
-          live: true
-        }),
-        pull.drain(function (a) {
-          if(a.sync) {
-            ready = true
-            while(waiting.length) waiting.shift()()
-            return
-          }
-          last = a.ts
-          if (a.image && typeof a.image === 'object') a.image = a.image.link
-          //set image for avatar.
-          //overwrite another avatar
-          //you picked.
-          if(
-            //if there is no avatar
-              (!avatars[a.id]) ||
-            //if i chose this avatar
-              (a.by == self_id) ||
-            //they chose their own avatar,
-            //and current avatar was not chosen by me
-              (a.by === a.id && avatars[a.id].by != self_id)
-          )
-            avatars[a.id] = a
-
-        })
-      )
-    },
-    avatar_image: function (author, classes) {
-      classes = classes || ''
-      if(classes && 'string' === typeof classes) classes = '.avatar--'+classes
-
-      function gen (id) {
-        if(cache[id]) return h('img', {src: cache[id]})
-        var img = visualize(new Buffer(author.substring(1), 'base64'), 256)
-        cache[id] = img.src
-        return img
+  pull(
+    query({
+      query: [{
+        $filter: {
+          timestamp: {$gt: last || 0 },
+          value: { content: {
+            type: "about",
+            about: {$prefix: "@"},
+            image: {$truthy: true}
+        }}
+      }},
+      {
+        $map: {
+          id: ["value", "content", "about"],
+          image: ["value", "content", "image"],
+          by: ["value", "author"],
+          ts: 'timestamp'
+      }}],
+      live: true
+    }),
+    pull.drain(function (a) {
+      if(a.sync) {
+        ready = true
+        while(waiting.length) waiting.shift()()
+        return
       }
+      last = a.ts
+      if (a.image && typeof a.image === 'object') a.image = a.image.link
+      //set image for avatar.
+      //overwrite another avatar
+      //you picked.
+      if(
+        //if there is no avatar
+          (!avatars[a.id]) ||
+        //if i chose this avatar
+          (a.by == self_id) ||
+        //they chose their own avatar,
+        //and current avatar was not chosen by me
+          (a.by === a.id && avatars[a.id].by != self_id)
+      )
+        avatars[a.id] = a
 
-      var img = ready && avatars[author] ? h('img', {src: blobUrl(avatars[author].image)}) : gen(author)
+    })
+  )
 
-      ;(classes || '').split('.').filter(Boolean).forEach(function (c) {
-        img.classList.add(c)
-      })
+  exports.avatar_image = function (author, classes) {
+    classes = classes || ''
+    if(classes && 'string' === typeof classes) classes = '.avatar--'+classes
 
-      if(!ready)
-        waiting.push(function () {
-          if(avatars[author]) img.src = blobUrl(avatars[author].image)
-        })
-
+    function gen (id) {
+      if(cache[id]) return h('img', {src: cache[id]})
+      var img = visualize(new Buffer(author.substring(1), 'base64'), 256)
+      cache[id] = img.src
       return img
     }
+
+    var img = ready && avatars[author] ? h('img', {src: blobUrl(avatars[author].image)}) : gen(author)
+
+    ;(classes || '').split('.').filter(Boolean).forEach(function (c) {
+      img.classList.add(c)
+    })
+
+    if(!ready)
+      waiting.push(function () {
+        if(avatars[author]) img.src = blobUrl(avatars[author].image)
+      })
+
+    return img
+    
   }
+
+  return exports
 }
 
