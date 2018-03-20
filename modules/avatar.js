8804ddb086b7b3a415b976fc3e54220f3ea0034c
Ev Bogue <ev@evbogue.com>
2018-03-19 22:20:28 -0500

    cache avatar names and images in memory

--- modules/avatar.js
+++ modules/avatar.js
@@ -6,16 +6,22 @@ var visualize = require('visualize-buffer')
 
 var config = require('../config')()
 
-module.exports.name = function (id) {
-  var name = h('span', id.substring(0, 10))
+var cache = {}
+var icache = {}
 
-  pull(query({query: [{$filter: { value: { author: id, content: {type: 'about', about: id, name: {'$truthy': true}}}}}], reverse: true}),
-    pull.collect(function (err, data){
-      if(data[0]) {
-        var data = data[0].value.content.name
-        name.textContent = '@' + data
-    }
-  }))
+module.exports.name = function (id) {
+  
+  var name = h('span', id.substring(0, 10))
+  if (cache[id])
+    name.textContent = cache[id]
+  else
+    pull(query({query: [{$filter: { value: { author: id, content: {type: 'about', about: id, name: {'$truthy': true}}}}}], reverse: true}),
+      pull.collect(function (err, data){
+        if(data[0]) {
+          cache[id] = '@' + data[0].value.content.name
+          name.textContent = cache[id]
+      }
+    }))
   return name
 }
 
@@ -23,22 +29,25 @@ var ref = require('ssb-ref')
 
 module.exports.image = function (id) {
   var img = visualize(new Buffer(id.substring(1), 'base64'), 256)
-  pull(query({query: [{$filter: { value: { author: id, content: {type: 'about', about: id, image: {'$truthy': true}}}}}], reverse: true}),
-    pull.collect(function (err, data){
-      if(data[0]) {
-        console.log(data[0])
-        if (ref.isBlob(data[0].value.content.image.link)) { 
-          var data = config.blobsUrl + data[0].value.content.image.link
-          console.log(data)
-          img.src = data
-        } else if (ref.isBlob(data[0].value.content.image)) {
-          var data = config.blobsUrl + data[0].value.content.image
-          console.log(data)
-          img.src = data
+
+  if (icache[id])
+    img.src = icache[id]
+  else
+    pull(query({query: [{$filter: { value: { author: id, content: {type: 'about', about: id, image: {'$truthy': true}}}}}], reverse: true}),
+      pull.collect(function (err, data){
+        if(data[0]) {
+          if (ref.isBlob(data[0].value.content.image.link)) { 
+            var data = config.blobsUrl + data[0].value.content.image.link
+            icache[id] = data
+            img.src = data
+          } else if (ref.isBlob(data[0].value.content.image)) {
+            var data = config.blobsUrl + data[0].value.content.image
+            icache[id] = data
+            img.src = data
+          }
         }
-      }
-    })
-  )
+      })
+    )
 
   return img
 }
