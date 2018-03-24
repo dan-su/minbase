3a45f871adcf3f53bbeccb8ea2a5b4bae67897d4
Ev Bogue <ev@evbogue.com>
2018-03-23 22:19:47 -0500

    only show editor on posts you authored

--- modules/post.js
+++ modules/post.js
@@ -5,7 +5,7 @@ var markdown = require('./helpers').markdown
 var pull = require('pull-stream')
 
 var query = require('./scuttlebot').query
-
+var self_id = require('./../keys')
 var timestamp = require('./helpers').timestamp
 
 exports.gives = 'message_content'
@@ -26,7 +26,7 @@ exports.create = function (api) {
       edited: data.key
     }
 
-    var editor = h('div.editor', api.message_compose(meta, {text: data.value.content.text})) 
+    var editor = h('div.editor', api.message_compose(meta, {text: data.value.content.text}))
 
     var message = h('div', re, h('div.innercontent', h('span.edited', 'Unedited'), markdown(data.value.content.text), editor))
 
@@ -34,15 +34,17 @@ exports.create = function (api) {
       pull.collect(function (err, data){
         if(data[0]) {
           for (var i = 0; i < data.length; i++) {
-            edited = data[i]
-            console.log(edited.key)
+            data = data[i]
+            if (data.value.author == self_id)
+              var editeditor = editor 
+            else 
+              var editeditor = '' 
+            console.log(data.key)
             message.appendChild(
               h('div.innercontent', 
-                h('span.edited', 
-                  'Edited ', 
-                  timestamp(edited)), 
-                  markdown(edited.value.content.text),
-                  api.message_compose(meta, {text: edited.value.content.text})
+                h('span.edited', 'Edited ', timestamp(data)), 
+                markdown(data.value.content.text),
+                editeditor
               )
             )
           }
