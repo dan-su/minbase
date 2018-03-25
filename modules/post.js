3ee0398629a78832c2fbe78831ce756faeaeca74
Ev Bogue <ev@evbogue.com>
2018-03-25 18:27:24 -0500

    only show edit dialog on messages you own

--- modules/post.js
+++ modules/post.js
@@ -5,7 +5,7 @@ var markdown = require('./helpers').markdown
 var pull = require('pull-stream')
 
 var query = require('./scuttlebot').query
-var self_id = require('./../keys')
+var id = require('./../keys').id
 var timestamp = require('./helpers').timestamp
 
 exports.gives = 'message_content'
@@ -17,35 +17,45 @@ exports.create = function (api) {
     if(!data.value.content || !data.value.content.text) return
     if(data.value.content.type === 'edit') return
 
-    var root = data.value.content.root
-    var re = !root ? null : h('span', 're: ', messageLink(root))
-    console.log(data)
+    function re (data) {
+      var root = data.value.content.root
+      if (root)
+        return h('span', 're: ', messageLink(root))
+    }
 
     var meta = {
       type: 'edit',
       edited: data.key
     }
 
-    var editor = h('div.editor', api.message_compose(meta, {text: data.value.content.text}))
+    var original = data
 
-    var message = h('div', re, h('div.innercontent', h('span.edited', 'Unedited'), markdown(data.value.content.text), editor))
+    function getEditor (data) {
+      if (data.value.author == id) {
+        return h('span.editor', api.message_compose(meta, {text: data.value.content.text}))
+      } else { return h('span.editor','')}
+    }
+
+    var message = h('div', re(data), h('div.innercontent', h('span.edited', 'Unedited'), markdown(data.value.content.text)))
 
     pull(query({query: [{$filter: { value: { author: data.value.author, content: {type: 'edit', edited: data.key}}}}], limit: 100}),
       pull.collect(function (err, data){
         if(data[0]) {
-          for (var i = 0; i < data.length; i++) {
+          var fin = data.length
+          for (var i = 0; i < fin; i++) {
             edited = data[i]
-            console.log(edited.key)
             message.appendChild(
               h('div.innercontent',
                 h('span.edited',
                   'Edited ',
                   timestamp(edited)),
-                  markdown(edited.value.content.text),
-                  api.message_compose(meta, {text: edited.value.content.text})
+                  markdown(edited.value.content.text)
               )
             )
           }
+          message.appendChild(getEditor(data[fin - 1]))
+        } else {
+          message.appendChild(getEditor(original))
         }
       })
     )
